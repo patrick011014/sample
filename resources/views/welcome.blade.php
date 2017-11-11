870fa8336a5883151c58d75db2f66613cc3aee5e
Guillermo Tabligan <gtplus.net@gmail.com>
2017-11-11 02:45:50 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Records</title>
+</head>
+<body>
+
+<input type="text" name="fname" placeholder="First name"><br>
+<input type="text" name="lname" placeholder="Last name"><br>
+<input type="text" name="section" placeholder="section"><br>
+<select>
+    <option>Year Level</option>
+    <option value="first year">1st Year</option>
+    <option value="second year">2nd Year</option>
+    <option value="third year">3rd Year</option>
+    <option value="fourth year">4th Year</option>
+</select>
+
+<button id="add">Add students</button>
+
+
+</body>
+</html>
